2a9cf02b97f9f705d747b3806eeba2db6d0a64ff
Priyanshu Singh <priyanshutech20@gmail.com>
2024-04-25 16:41:11 +0530

    Update Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,13 +6,27 @@ import {
   FormControl,
   FormLabel,
 } from "@mui/material";
+import MathJax from "react-mathjax";
 import "../styles/Question.css";
 
 function Question({ question, handleAnswer, currentAnswer, index }) {
+  // Function to check and prepare the text for MathJax rendering
+  const prepareMathText = (text) => {
+    // This regex replaces all instances of math-related expressions with LaTeX compatible versions
+    const updatedText = text.replace(
+      /(\d+)\.x\.y\^(\d+)\.z/g,
+      `\$-\$9xy^{$2}z`
+    );
+    return `$$${updatedText}$$`; // Wrap with $$ for MathJax
+  };
+
   return (
     <FormControl component="fieldset" className="question-container">
       <FormLabel component="legend" className="question-title">
-        <strong>Q{index + 1}.</strong> {question.Question}
+        <strong>Q{index + 1}.</strong>{" "}
+        <MathJax.Provider>
+          <MathJax.Node inline formula={prepareMathText(question.Question)} />
+        </MathJax.Provider>
       </FormLabel>
       <RadioGroup
         value={currentAnswer}
@@ -23,7 +37,11 @@ function Question({ question, handleAnswer, currentAnswer, index }) {
             key={optionIndex}
             value={optionIndex}
             control={<Radio />}
-            label={option}
+            label={
+              <MathJax.Provider>
+                <MathJax.Node inline formula={option} />
+              </MathJax.Provider>
+            }
             className={`option ${
               currentAnswer === optionIndex ? "selected" : ""
             }`}
